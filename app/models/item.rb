ee5db06d5afbe552ae4491e04c1576ef52623f95
Katie Keel <akatiekeel@gmail.com>
2017-07-26 08:06:39 -0600

    Put all validations on same line for Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,5 @@
 class Item < ApplicationRecord
-  validates_presence_of :title
-  validates_presence_of :description
-  validates_presence_of :price
-  validates_presence_of :image_path
+  validates_presence_of :title, :description, :price, :image_path
   has_many :category_items
   has_many :categories, through: :category_items
 end
