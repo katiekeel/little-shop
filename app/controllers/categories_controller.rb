51904e3b669444612f32f4042fc4a18a87a991a5
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-26 08:49:03 -0600

    Items index page has basic styling; about.html inside categories is temporary demonstration of layout template

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,13 @@
 class CategoriesController < ApplicationController
 
+  def about
+
+  end
+  
   def show
     @category = Category.find_by(title: params[:category_title])
     @items = @category.items
   end
+
+
 end
