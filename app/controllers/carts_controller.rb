ad283ff876feee02ee3ed86c19a1199ce2604397
Katie Shermer <katieshermer@gmail.com>
2017-07-26 16:49:16 -0600

    It might work

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,7 @@ class CartsController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def index
+    byebug
     @items = Item.find(@cart.contents.keys)
   end
 
@@ -9,6 +10,16 @@ class CartsController < ApplicationController
     item = Item.find(params[:item_id])
 
     @cart.add_item(item.id)
+    session[:cart] = @cart.contents
+
+    flash[:notice] = "You now have #{pluralize(session[:cart][item.id.to_s], item.title)}."
+    redirect_to items_path
+  end
+
+  def delete
+    item = Item.find(params[:item_id])
+
+    @cart.delete_item(item.id)
     session[:cart] = @cart.contents
 
     flash[:notice] = "You now have #{pluralize(session[:cart][item.id.to_s], item.title)}."
