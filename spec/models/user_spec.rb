7344b9e2a214a51787f8c1a436a59fc936643af1
Katie Shermer <katieshermer@gmail.com>
2017-07-28 08:28:25 -0600

    User model validations tests done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+  end
 end
