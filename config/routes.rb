84a87617a91a8dcee9b3f988b83566e7e1e6fc62
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-28 17:32:25 -0600

    added orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :orders, only: [:index]
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
--- spec/features/authenticated/user_can_view_their_past_orders_spec.rb
+++ spec/features/authenticated/user_can_view_their_past_orders_spec.rb
@@ -1,9 +1,11 @@
 RSpec.feature "a user can see their past orders" do
   scenario "when they visit /orders" do
     user = create(:user)
-    order_1 = create(order, user_id: user.id)
-    order_2 = create(order, user_id: user.id)
-    order_3 = create(order, user_id: user.id)
+    user_2 = create(:user)
+    order_1 = create(:order, user_id: user.id)
+    order_2 = create(:order, user_id: user.id)
+    order_3 = create(:order, user_id: user.id)
+    order_4 = create(:order, user_id: user_2.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -12,5 +14,6 @@ RSpec.feature "a user can see their past orders" do
     expect(page).to have_content("order-#{order_1.id}")
     expect(page).to have_content("order-#{order_2.id}")
     expect(page).to have_content("order-#{order_3.id}")
+    expect(page).to_not have_content("order-#{order_4.id}")
   end
 end
