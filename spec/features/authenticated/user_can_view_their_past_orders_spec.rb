cc6d94b1b5713b3718823028ae56f66f85d41851
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-28 16:35:08 -0600

    initial user story built out

---
+++ spec/features/authenticated/user_can_view_their_past_orders_spec.rb
@@ -0,0 +1,12 @@
+RSpec.feature "a user can see their past orders" do
+  scenario "when they visit /orders" do
+    #Background: An existing user that has multiple orders
+
+    #As an Authenticated User
+
+    #When I visit "/orders"
+
+    #Then I should see all orders belonging to me and no other orders
+
+  end
+end
