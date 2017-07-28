c32f9ffb40e39771abb0fe9e8a5bdb388bb81495
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-28 17:41:56 -0600

    updated user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
   end
+
+  context "relationships" do
+    it {should have_many(:orders) }
+  end
 end
