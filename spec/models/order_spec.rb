929457ba7c7fdb2b62a4b38e5855959205f6fb7b
Ellen Cooper <ellencooper74@gmail.com>
2017-07-29 13:32:01 -0600

    adds model test for .total_price method on order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Order, type: :model do
     it {should have_many(:item_orders) }
     it {should have_many(:items) }
   end
+  context "instance methods" do
+    it ".total_price returns total price of order" do
+      item_1, item_2 = create_list(:item, 2, price: 5)
+      user = create(:user)
+      order_1 = create(:order, user_id: user.id, status: 3)
+      create(:item_order, item: item_1, order: order_1, quantity: 1)
+      create(:item_order, item: item_2, order: order_1, quantity: 2)
+
+      expect(order_1.total_price).to eq(15)
+    end
+  end
 end
