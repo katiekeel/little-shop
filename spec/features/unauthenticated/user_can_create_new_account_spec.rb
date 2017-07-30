dbf41ef65d41382bd20b537009ebe652231eba99
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-30 15:11:05 -0600

    sad path testing complete for creating account

--- spec/features/unauthenticated/user_can_create_new_account_spec.rb
+++ spec/features/unauthenticated/user_can_create_new_account_spec.rb
@@ -28,4 +28,74 @@ RSpec.feature "User can create new account" do
 
     expect(page).to have_link("Logout")
   end
+
+  scenario "user enters unavailable username" do
+    create_list(:item, 3)
+    user = create(:user, username: "sauron")
+
+    visit(root_path)
+
+    expect(page).to have_link("Login")
+
+    click_link("Login")
+
+    expect(current_path).to eq(login_path)
+
+    click_link("Create Account")
+
+    fill_in "user[username]", with: "sauron"
+    fill_in "user[password]", with: "Bilbo123"
+    fill_in "user[password_confirmation]", with: "Bilbo123"
+
+    click_button("Signeth Thou Up")
+
+    expect(page).to have_content("Username doth taken or passwordeth no valid, please retryith")
+    expect(page).to_not have_content("Signed in as sauron")
+  end
+
+  scenario "user enters invalid password" do
+    create_list(:item, 3)
+
+    visit(root_path)
+
+    expect(page).to have_link("Login")
+
+    click_link("Login")
+
+    expect(current_path).to eq(login_path)
+
+    click_link("Create Account")
+
+    fill_in "user[username]", with: "sauron"
+    fill_in "user[password]", with: ""
+    fill_in "user[password_confirmation]", with: ""
+
+    click_button("Signeth Thou Up")
+
+    expect(page).to have_content("Username doth taken or passwordeth no valid, please retryith")
+    expect(page).to_not have_content("Signed in as sauron")
+  end
+
+  scenario "user enters incorrect password confirmation" do
+    create_list(:item, 3)
+
+    visit(root_path)
+
+    expect(page).to have_link("Login")
+
+    click_link("Login")
+
+    expect(current_path).to eq(login_path)
+
+    click_link("Create Account")
+
+    fill_in "user[username]", with: "sauron"
+    fill_in "user[password]", with: "Bilbo123"
+    fill_in "user[password_confirmation]", with: "not bilbo123"
+
+    click_button("Signeth Thou Up")
+
+    expect(page).to have_content("Username doth taken or passwordeth no valid, please retryith")
+    expect(page).to_not have_content("Signed in as sauron")
+  end
 end
