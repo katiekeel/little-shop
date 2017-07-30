a02011de87e288a0e8963852ba22fb9ecbb76b84
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-30 15:21:39 -0600

    carts controller dryed up

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,38 +1,43 @@
 class CartsController < ApplicationController
   include ActionView::Helpers::TextHelper
+  before_action :find_item, only: [:create, :destroy, :update]
 
   def index
     @items = Item.find(@cart.contents.keys)
   end
 
   def create
-    item = Item.find(params[:item_id])
-
-    @cart.add_item(item.id)
+    @cart.add_item(@item.id)
     session[:cart] = @cart.contents
-
-    flash[:notice] = "You now have #{pluralize(session[:cart][item.id.to_s], item.title)}."
+    flash[:notice] = "You now have #{pluralize(session[:cart][@item.id.to_s], @item.title)}."
     redirect_back(fallback_location: items_path)
   end
 
   def destroy
-    item = Item.find(params[:item_id])
-    @cart.delete_item(item.id)
+    delete_from_cart
     session[:cart] = @cart.contents
-    flash[:notice] = "You have successfully removed <a href='/items/#{item.id}'>#{item.title}</a> from the cart."
+    flash[:notice] = "You have successfully removed <a href='/items/#{@item.id}'>#{@item.title}</a> from the cart."
     redirect_to cart_path
   end
 
   def update
-    item = Item.find(params[:item_id])
     if params[:quantity] == "0"
-      @cart.delete_item(item.id)
+      delete_from_cart
     else
-      @cart.update_item(item.id, params[:quantity])
+      @cart.update_item(@item.id, params[:quantity])
     end
     session[:cart] = @cart.contents
-
-    flash[:notice] = "You now have #{pluralize(session[:cart][item.id.to_s], item.title)}."
+    flash[:notice] = "You now have #{pluralize(session[:cart][@item.id.to_s], @item.title)}."
     redirect_back(fallback_location: items_path)
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def delete_from_cart
+    @cart.delete_item(@item.id)
   end
 end
