26eef89ec5d34d3e9113d025cffc3e7442dfaaf8
Katie Shermer <katieshermer@gmail.com>
2017-07-30 11:14:35 -0600

    Fixed Story 14 as per Slack discussion with Mike

--- spec/features/user_sees_retired_item_spec.rb
+++ spec/features/user_sees_retired_item_spec.rb
@@ -14,5 +14,14 @@ describe "User sees retired item and" do
     # And I should see in place of the "Add to Cart" button or link - "Item Retired"
     expect(page).to_not have_button "Add to Cart"
     expect(page).to have_content "Item Retired"
+
+    visit items_path
+
+    expect(current_path).to eq "/items"
+    expect(page).to have_content "#{item.title}"
+    expect(page).to have_content "#{item.description}"
+
+    expect(page).to_not have_button "Add to Cart"
+    expect(page).to have_content "Item Retired"
   end
 end
