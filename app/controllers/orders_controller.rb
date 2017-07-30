a5192bc61b8a7b07764f7a5f23659818ce76b45b
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-30 11:34:08 -0600

    order creation passes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,11 @@ class OrdersController < ApplicationController
   before_action :find_order, only: [:show]
 
   def create
-    byebug
-
+    @order = Order.new(user: current_user)
+    @cart.contents.each do |item, qty|
+      @order.item_orders.new(item_id: item.to_i, quantity: qty)
+    end
+    @order.save
     redirect_to orders_path
   end
 
