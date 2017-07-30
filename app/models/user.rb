665705e3d25d2faf410e9d66b2b188a06ccb4523
Katie Shermer <katieshermer@gmail.com>
2017-07-30 15:44:07 -0600

    User dashboard built out with most ordered item ugly, but working

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   validates_uniqueness_of :username
 
   has_many :orders
+
+  def most_ordered_item
+    orders = Order.where(user: self).pluck(:id)
+    order_items = ItemOrder.where(order: orders).pluck(:item_id)
+    items = Item.where(id: order_items).group(:title).count
+    item = Item.find(items.values.max)
+  end
 end
