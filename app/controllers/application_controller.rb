76d0d6143db91cd2ce141908e0b728cfe9f010f0
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-31 17:32:40 -0600

    added conditionals for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
-  helper_method :current_user
+  helper_method :current_user, :current_admin?
 
   def set_cart
     @cart ||= Cart.new(session[:cart])
