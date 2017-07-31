e8e8e6153f1622a05bf45731b7d629ae6e6fd261
JoelLindow <joellindow@gmail.com>
2017-07-31 10:48:14 -0600

    cleanup test

--- spec/features/admin/user_cant_see_admin_dashboard_spec.rb
+++ spec/features/admin/user_cant_see_admin_dashboard_spec.rb
@@ -1,21 +1,12 @@
 RSpec.feature "User tried to go to admin dashboard" do
   scenario "gets a gangalfy 404 error" do
-    # As a registered user
+
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    # When I visit "/admin/dashboard"
+
     visit admin_dashboard_path
-    # I get a 404
+
     expect(page).to have_content("404")
     expect(page).to have_content("You shall not pass!")
-save_and_open_page
-    # As an unregistered user
-
-    # When I visit "/admin/dashboard"
-
-    # I get a 404
-
-
-
   end
 end
