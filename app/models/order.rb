2db02d3ccb9917888e2f867530a3339ab6f7639b
Katie Shermer <katieshermer@gmail.com>
2017-07-31 16:50:02 -0600

    Story 20/21 working

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,4 +9,20 @@ class Order < ApplicationRecord
     ItemOrder.joins(:item).where("order_id = ?", self.id).sum("price * quantity")
   end
 
+  def ordered
+    self.where(status: "ordered")
+  end
+
+  def paid
+    self.where(status: "paid")
+  end
+
+  def cancelled
+    self.where(status: "cancelled")
+  end
+
+  def completed
+    self.where(status: "completed")
+  end
+
 end
--- spec/features/admin/admin_can_see_all_orders_spec.rb
+++ spec/features/admin/admin_can_see_all_orders_spec.rb
@@ -1,13 +1,13 @@
-RSpec.feature "Admin visits dashboard" do
+RSpec.feature "Admin visits dashboard and" do
   scenario "can see all orders" do
     # As an Admin
     admin = create(:user, role: "admin")
     user  = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-    create_list(:order, 5, :with_item_orders, user: user)
-    create_list(:order, 6, :with_item_orders, user: user, status: "paid")
-    create_list(:order, 7, :with_item_orders, user: user, status: "cancelled")
-    create_list(:order, 8, :with_item_orders, user: user, status: "completed")
+    ordered = create_list(:order, 5, user: user)
+    paid = create_list(:order, 6, user: user, status: "paid")
+    cancelled = create_list(:order, 7, user: user, status: "cancelled")
+    completed = create_list(:order, 8, user: user, status: "completed")
 
     # When I visit the dashboard
     visit admin_dashboard_path
@@ -20,22 +20,73 @@ RSpec.feature "Admin visits dashboard" do
     expect(page).to have_content("Paid (6)")
     expect(page).to have_content("Cancelled (7)")
     expect(page).to have_content("Completed (8)")
-
     # And I can see a link for each individual order
-
     # And I can filter orders to display by each status type ("Ordered", "Paid", "Cancelled", "Completed")
+    within("#orders") do
+      expect(page).to have_link("order-#{Order.first.id}")
+    end
 
-    # And I have links to transition between statuses
+    within("#ordered-orders") do
+      expect(page).to have_link("order-#{ordered.first.id}")
+    end
+
+    within("#paid-orders") do
+      expect(page).to have_link("order-#{paid.first.id}")
+    end
+
+    within("#cancelled-orders") do
+      expect(page).to have_link("order-#{cancelled.first.id}")
+    end
+
+    within("#completed-orders") do
+      expect(page).to have_link("order-#{completed.first.id}")
+    end
+  end
+
+  scenario "can change order status" do
+    admin = create(:user, role: "admin")
+    user  = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    ordered = create_list(:order, 5, user: user)
+    paid = create_list(:order, 6, user: user, status: "paid")
+    cancelled = create_list(:order, 7, user: user, status: "cancelled")
+    completed = create_list(:order, 8, user: user, status: "completed")
+
+    visit admin_dashboard_path
 
     # I can click on "cancel" on individual orders which are "paid" or "ordered"
+    within("#paid-orders") do
+      click_link_or_button("Cancel", :match => :first)
+    end
+
+    within("#cancelled-orders") do
+      expect(page).to have_link "order-#{paid.first.id}"
+    end
+
+    within("#ordered-orders") do
+      click_link_or_button("Cancel", :match => :first)
+    end
+
+    within("#cancelled-orders") do
+      expect(page).to have_link "order-#{ordered.first.id}"
+    end
 
     # I can click on "mark as paid" on orders that are "ordered"
+    within("#ordered-orders") do
+      click_link_or_button("Mark As Paid", :match => :first)
+    end
+
+    within("#paid-orders") do
+      expect(page).to have_link "order-#{ordered.second.id}"
+    end
 
     # I can click on "mark as completed" on orders that are "paid"
+    within("#paid-orders") do
+      click_link_or_button("Mark As Completed", :match => :first)
+    end
 
-
-
-
-
+    within("#completed-orders") do
+      expect(page).to have_content "order-#{paid.second.id}"
+    end
   end
 end
