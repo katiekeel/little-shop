cbe08d3692762d6dd8cef79417e7f8b4caa97e0a
Ellen Cooper <ellencooper74@gmail.com>
2017-07-30 19:01:45 -0600

    changed update cart quantity route to patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,25 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :show]
-
   resources :carts, only: [:create]
-
   resources :users, only: [:new, :create]
-
   resources :orders, only: [:index, :show, :create]
 
   get '/edit_account', to: 'users#edit'
   patch '/update_account', to: 'users#update'
   delete '/delete_account', to: 'users#destroy'
+  get '/dashboard', to: 'users#show'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-
   delete '/logout', to: 'sessions#destroy'
 
-  post '/cart', to: "carts#update"
+  patch '/cart', to: "carts#update"
   delete '/cart', to: "carts#destroy"
-
   get '/cart', to: 'carts#index'
-
-  get '/dashboard', to: 'users#show'
 
   get '/admin', to: 'sessions#new'
 
   get '/about', to: 'categories#about'
-
   get '/:category_title', to: 'categories#show'
 
   root to: 'welcome#index'
