90c3c72bd09f09a3280c0cbdda6c846a1f05b2a3
Ellen Cooper <ellencooper74@gmail.com>
2017-08-02 13:49:34 -0600

    adds validation tests for price and title for item"

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   context "Validations" do
     it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:price) }
     it { should validate_presence_of(:image_path) }
     it { should validate_presence_of(:categories) }
+    it { should validate_numericality_of(:price)}
+    it { should_not allow_value([0, -1, "A", "1"]).for(:price) }
   end
 
   context "Relationships" do
