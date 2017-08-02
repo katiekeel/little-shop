3d2a491ccbf95d73af1386766b45affc9264845f
Matt DeVoe <matthew.devoe@gmail.com>
2017-08-02 13:50:40 -0600

    updating tests to pass

--- spec/features/unauthenticated/user_adds_item_to_cart_spec.rb
+++ spec/features/unauthenticated/user_adds_item_to_cart_spec.rb
@@ -4,7 +4,7 @@ describe "User visits items index page and" do
 
     visit '/items'
 
-    click_button("Add to Cart")
+    find(:css, ".index-cart").click
 
     expect(page).to have_content "You now have 1 #{item.title}."
 
@@ -15,7 +15,7 @@ describe "User visits items index page and" do
     expect(page).to have_content item.title
     expect(page).to have_content item.description
     expect(page).to have_content item.price.to_f.round(2)
-    expect(page).to have_css("img", count: 1)
+    expect(page).to have_css("img", count: 2)
     expect(page).to have_content "Cart Total: $#{item.price.to_f.round(2)}"
   end
 end
