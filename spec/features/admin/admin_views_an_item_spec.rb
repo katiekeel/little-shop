c751da5802983457396507579c1c79de9cdd9ed7
Katie Shermer <katieshermer@gmail.com>
2017-08-02 13:13:59 -0600

    Admin item show tests pass

--- spec/features/admin/admin_views_an_item_spec.rb
+++ spec/features/admin/admin_views_an_item_spec.rb
@@ -13,5 +13,30 @@ describe "Admin views an individual item" do
     within("tbody tr[1]") do
       click_link("#{item_1.title}")
     end
+
+    expect(current_path).to eq "/admin/items/#{item_1.id}"
+    expect(page).to have_content item_1.status.capitalize
+    expect(page).to have_link "Edit"
+  end
+
+  context "and clicks its edit button" do
+    scenario "and is routed to its edit page" do
+      item_1, item_2 = create_list(:item, 2)
+      admin = create(:user, role: "admin")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_path
+
+      click_on "View Thine Items"
+
+      expect(current_path).to eq("/admin/items")
+
+      within("tbody tr[1]") do
+        click_link("Edit")
+      end
+
+      expect(current_path).to eq "/admin/items/#{item_1.id}/edit"
+      expect(page).to have_content "Edit Thy Item"
+    end
   end
 end
