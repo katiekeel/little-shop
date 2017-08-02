7bf04d70e0c33409b5d1da3c616c7220b0ab0922
Katie Shermer <katieshermer@gmail.com>
2017-08-02 14:24:06 -0600

    Changed it back to original

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe User, type: :model do
   context "relationships" do
     it {should have_many(:orders) }
   end
+
+  context "instance methods" do
+    it "most ordered item returns most orderd item for user" do
+      item_1, item_2 = create_list(:item, 2, price: 5)
+      user = create(:user)
+      order_1 = create(:order, user_id: user.id, status: 3)
+      create(:item_order, item: item_1, order: order_1, quantity: 1)
+      create(:item_order, item: item_1, order: order_1, quantity: 1)
+      create(:item_order, item: item_2, order: order_1, quantity: 2)
+      create(:item_order, item: item_2, order: order_1, quantity: 2)
+      create(:item_order, item: item_2, order: order_1, quantity: 2)
+      create(:item_order, item: item_2, order: order_1, quantity: 2)
+      create(:item_order, item: item_2, order: order_1, quantity: 2)
+
+      expect(user.most_ordered_item).to eq item_2
+    end
+  end
 end
