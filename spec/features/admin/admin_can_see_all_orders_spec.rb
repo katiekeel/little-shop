2da4a04143a82d695886add236c2894d43eb7812
Ellen Cooper <ellencooper74@gmail.com>
2017-08-02 19:22:15 -0600

    removes comments from tests

--- spec/features/admin/admin_can_see_all_orders_spec.rb
+++ spec/features/admin/admin_can_see_all_orders_spec.rb
@@ -1,6 +1,5 @@
 RSpec.feature "Admin visits dashboard and" do
   scenario "can see all orders" do
-    # As an Admin
     admin = create(:user, role: "admin")
     user  = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -9,19 +8,15 @@ RSpec.feature "Admin visits dashboard and" do
     cancelled = create_list(:order, 7, user: user, status: "cancelled")
     completed = create_list(:order, 8, user: user, status: "completed")
 
-    # When I visit the dashboard
     visit admin_dashboard_path
-    # Then I can see a listing of all orders
     expect(page).to have_content("All Orders (26)")
     expect(page).to have_link("order-#{Order.first.id}")
     expect(page).to have_link("order-#{Order.last.id}")
-    # And I can see the total number of orders for each status ("Ordered", "Paid", "Cancelled", "Completed")
     expect(page).to have_content("Ordered (5)")
     expect(page).to have_content("Paid (6)")
     expect(page).to have_content("Cancelled (7)")
     expect(page).to have_content("Completed (8)")
-    # And I can see a link for each individual order
-    # And I can filter orders to display by each status type ("Ordered", "Paid", "Cancelled", "Completed")
+
     within("#orders") do
       expect(page).to have_link("order-#{Order.first.id}")
     end
@@ -54,7 +49,6 @@ RSpec.feature "Admin visits dashboard and" do
 
     visit admin_dashboard_path
 
-    # I can click on "cancel" on individual orders which are "paid" or "ordered"
     within("#paid-orders") do
       click_link_or_button("Cancel", :match => :first)
     end
@@ -71,7 +65,6 @@ RSpec.feature "Admin visits dashboard and" do
       expect(page).to have_link "order-#{ordered.first.id}"
     end
 
-    # I can click on "mark as paid" on orders that are "ordered"
     within("#ordered-orders") do
       click_link_or_button("Mark As Paid", :match => :first)
     end
@@ -80,7 +73,6 @@ RSpec.feature "Admin visits dashboard and" do
       expect(page).to have_link "order-#{ordered.second.id}"
     end
 
-    # I can click on "mark as completed" on orders that are "paid"
     within("#paid-orders") do
       click_link_or_button("Mark As Completed", :match => :first)
     end
